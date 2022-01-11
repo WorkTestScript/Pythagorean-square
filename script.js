c8f047db1b4e03adad9856843139d1731e240850
GlobalScript <sandr.onlooker@gmail.com>
2022-01-11 16:51:32 +0200

    comit

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-const selectDiv = document.querySelector('.select-date');
-const select = document.createElement('select');
-const option = document.createElement('option');
+const selectDay = document.querySelector('.select-day');
+const selectMonth = document.querySelector('.select-month');
+const selectYear = document.querySelector('.select-year');
+const showDate = document.querySelector('.show-date');
+const btnSelect = document.querySelector('.btn-select');
+const fullYear = (new Date()).getFullYear();
+{
+	let resultDay = '';
+	let resultMonth = '';
+	let resultYear = '';
+	for(let i = 1; i <= 31; i++){
+		resultDay += `<option value="${i}">${i}</option>`;
+	}
+	for(let i = 1; i <= 12; i++){
+		resultMonth += `<option value="${i}">${i}</option>`;
+	}
+	for(let i = 1900; i <= fullYear; i++){
+		resultYear += `<option value="${i}">${i}</option>`;
+	}
+	selectDay.innerHTML = resultDay;
+	selectMonth.innerHTML = resultMonth;
+	selectYear.innerHTML = resultYear;
+}
+function pushShowDate(){
+	showDate.textContent = (
+		`${selectDay.value < 10 ? "0" + selectDay.value : selectDay.value}.
+		 ${selectMonth.value < 10 ? "0" + selectMonth.value : selectMonth.value}.
+		 ${selectYear.value}`);
+}
+btnSelect.addEventListener('click', pushShowDate);
 
-console.log("Hello");
+
+
+
+
+
 
