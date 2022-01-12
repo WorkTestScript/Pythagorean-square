f654a2e22f76f54381b262a84488efc999530c7c
GlobalScript <sandr.onlooker@gmail.com>
2022-01-12 16:48:33 +0200

    fff

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const selectDay = document.querySelector('.select-day');
 const selectMonth = document.querySelector('.select-month');
 const selectYear = document.querySelector('.select-year');
 const showDate = document.querySelector('.show-date');
+const numberLife = document.querySelector('.number-life');
 const btnSelect = document.querySelector('.btn-select');
 const fullYear = (new Date()).getFullYear();
 {
@@ -26,10 +27,14 @@ function pushShowDate(){
 		`${selectDay.value < 10 ? "0" + selectDay.value : selectDay.value}.
 		 ${selectMonth.value < 10 ? "0" + selectMonth.value : selectMonth.value}.
 		 ${selectYear.value}`);
+	let sumDate = selectDay.value + selectMonth.value + selectYear.value;
+	let oneNum = sumDate.split('').reduce((accum, elem) => +accum + +elem);
+	let sumOneNum = String(oneNum).split('').reduce((accum, elem) => +accum + +elem);
+		numberLife.textContent = `Number life ${sumOneNum}` ;
 }
 btnSelect.addEventListener('click', pushShowDate);
 
-
+// https://matrizasudba.com/blog/numerologiya-pifagora-v-povsednevnoj-zhizni?gclid=CjwKCAiAlfqOBhAeEiwAYi43F_X2TQj7JvTnWTmk6EKNsCwgfCi1Bjfx3Pq9f07o9srtLWP52pHsMRoCHeAQAvD_BwE
 
 
 
